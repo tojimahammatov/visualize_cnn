995f06f6a51cffdebae55d1026004f4ce1d6918a
tojimahammatov <tojimahammatov@gmail.com>
2021-03-11 18:16:37 +0900

    add random filter

--- app/src/main/java/com/example/visualize_cnn/FiltersBank.java
+++ app/src/main/java/com/example/visualize_cnn/FiltersBank.java
@@ -8,5 +8,6 @@ public class FiltersBank {
     public double[][] EDGE = {{-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1}};
     public double[][] SHARPEN = {{0, -1, 0}, {-1, 5, -1}, {0, -1, 0}};
     public double[][] UNSHARP = {{-1/16, -1/8, -1/16}, {-1/8, 1/4, -1/8}, {-1/16, -1/8, -1/16}};
+    public double[][] RANDOM = {{0.123, 0.021, 0.09}, {0.001, 0.21, 0.03}, {0.1, 0.04, 0.007}};
 
 }
